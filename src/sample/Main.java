4ee2ecf6a353bbf191c985a6bf76e40846e036c9
lukasz <lukaszflorek22@wp.pl>
2021-06-07 17:38:41 +0200

    Zad 3 Począdkowy schemat poruszania piłką

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,8 +11,31 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
+
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 700;
+    private static final double MARGIN = 50;
+    private static final double ARENAWIDTH = WIDTH - 2*MARGIN;
+    private static final double ARENAHEIGHT = HEIGHT - 2*MARGIN;
+    private static final double ARENAX1 =  MARGIN;
+    private static final double ARENAY1 = MARGIN;
+    private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
+    private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
+    private static final double R = 10;
+    private double x = ARENAX1+ARENAWIDTH/2;
+
+    private void run(GraphicsContext gc)
+    {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(ARENAX1,ARENAY1,ARENAWIDTH,ARENAHEIGHT);
+
+        gc.setFill(Color.WHITESMOKE);
+        gc.fillOval(ARENAX1+ARENAWIDTH/2,ARENAY1+ARENAHEIGHT/2,2*R, 2*R);
+    }
+
 
     @Override
     public void start(Stage stage) throws Exception{
@@ -19,14 +44,18 @@ public class Main extends Application {
 //        primaryStage.setScene(new Scene(root, 300, 275));
 //        primaryStage.show();
 
-        Canvas canvas = new Canvas(800, 600);
+        Canvas canvas = new Canvas(WIDTH,HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100),e-> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
+
         stage.setTitle("Kulki");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
-        gc.setFill(Color.BLACK);
-        gc.fillRect(50,50,700,500);
+
+        t.play();
 
     }
 
